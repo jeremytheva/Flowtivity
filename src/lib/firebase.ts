e425e14439ef3ae95d3cf0a6c87bb61a8fd85732
Jeremy Uni <jeremymcdonall.uni@gmail.com>
2025-10-19 20:48:17 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || 'your-api-key',
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || 'your-auth-domain',
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || 'your-project-id',
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || 'your-storage-bucket',
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || 'your-messaging-sender-id',
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || 'your-app-id',
+  projectId: "studio-2615913698-17c08",
+  appId: "1:709307375131:web:ef682319693ea2b9dca2be",
+  apiKey: "AIzaSyBUABjFDR3oROvRvY2h7xWihsqNrypj19c",
+  authDomain: "studio-2615913698-17c08.firebaseapp.com",
+  messagingSenderId: "709307375131",
+  storageBucket: "studio-2615913698-17c08.appspot.com"
 };
 
 // Initialize Firebase
