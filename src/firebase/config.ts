7c584d830939334d90b8b4c883ba92d364a71af0
Jeremy Uni <jeremymcdonall.uni@gmail.com>
2025-10-20 03:36:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2615913698-17c08",
+  "appId": "1:709307375131:web:ef682319693ea2b9dca2be",
+  "apiKey": "AIzaSyBUABjFDR3oROvRvY2h7xWihsqNrypj19c",
+  "authDomain": "studio-2615913698-17c08.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "709307375131"
+};
